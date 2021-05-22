b9d2d7e9c07bd8c37a3c6302311780c9b9a57017
MFuku <8368409+Masaminr@users.noreply.github.com>
2021-05-22 13:38:44 +0900

    Initial Commit

---
+++ SwiftUI-Chapter2/SwiftUI_Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Chapter2App.swift
+//  SwiftUI-Chapter2
+//
+//  Created by 福井昌則 on 2021/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
